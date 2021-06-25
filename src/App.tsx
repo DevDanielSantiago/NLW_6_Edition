556b5200387f46d5782d43dc17bd79247eee593b
DevDanielSantiago <dev.danielsantiago@gmail.com>
2021-06-25 17:05:47 -0400

    aula 4

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Room } from "./pages/Room";
 import { NewRoom } from "./pages/NewRoom";
 
 import { AuthContextProvider } from "./contexts/AuthContext"
+import { AdminRoom } from "./pages/adminRoom";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
